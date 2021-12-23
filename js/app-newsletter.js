33edb13eb7c92ab0480b66e9261fef7ae81e3c03
luisforni <70423935+luisforni@users.noreply.github.com>
2021-12-23 02:06:13 -0300

    app1

---
+++ js/app-newsletter.js
@@ -0,0 +1,188 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 5,
+				slidesToScroll: 5
+			  }
+			}
+		]
+	});
+});
+
+$(document).ready(function () {
+
+	$('.titulo-1').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-1').text();
+			var resumenText = $('.resumen-1').text();
+			//$('.contenido-principal__imagen').attr('src','img/01.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-2').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-2').text();
+			var resumenText = $('.resumen-2').text();
+			//$('.contenido-principal__imagen').attr('src','img/02.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-3').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-3').text();
+			var resumenText = $('.resumen-3').text();
+			//$('.contenido-principal__imagen').attr('src','img/03.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-4').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-4').text();
+			var resumenText = $('.resumen-4').text();
+			//$('.contenido-principal__imagen').attr('src','img/04.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-5').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-5').text();
+			var resumenText = $('.resumen-5').text();
+			//$('.contenido-principal__imagen').attr('src','img/05.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-6').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-6').text();
+			var resumenText = $('.resumen-6').text();
+			//$('.contenido-principal__imagen').attr('src','img/06.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-7').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-7').text();
+			var resumenText = $('.resumen-7').text();
+			//$('.contenido-principal__imagen').attr('src','img/07.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+	$('.titulo-8').on({
+		'mouseover': function(){
+			var tituloText = $('.titulo-8').text();
+			var resumenText = $('.resumen-8').text();
+			//$('.contenido-principal__imagen').attr('src','img/08.jpg');
+			$('.contenido-principal__titulo').text(tituloText);
+			$('.contenido-principal__resumen').text(resumenText);
+		}
+	})
+
+});
+
+//-- TITULO --//
+
+var load = document.getElementById("loading");
+
+window.onload = lun;
+
+function lun() {
+    load.style.display = "none";
+}
+
+var dialog = document.getElementsByClassName("noticias-konecta")[0];
+var variableBorder = 1;
+var changeBorder = setInterval(change, 300);
+
+function change() {
+    if (variableBorder == 1) {
+        dialog.style.border = "none";
+        variableBorder = 2;
+    } else if (variableBorder == 2) {
+        dialog.style.borderRight = "0px solid #71bd46";
+        variableBorder = 1;
+    }
+}
+
+var dialogs = {
+    value0: function() {
+        texts = "Estamos";
+        timer = 100;
+    },
+    value1: function() {
+        texts = "KONECTADOS";
+        timer = 100;
+    },
+};
+
+var number = 0;
+var timer;
+var texts;
+
+type();
+
+function type() {
+    dialogs["value" + number]();
+    if (number >= 1) {
+        number = -1;
+    }
+    
+    dialog.innerHTML = "";
+    var Letters = [];
+    
+    for (var i = 0; i < texts.length; i++) {
+        text = texts.charAt(i);
+        Letters[i] = text;
+    }
+    
+    writes = setInterval(write, timer);
+    
+    positionLetter = 0;
+    
+    function write() {
+        if (positionLetter == Letters.length) {
+            clearInterval(writes);
+            number++;
+            if (number == Object.keys(dialogs).length) {
+                clearInterval(writes);
+                return up();
+            }
+            setTimeout(type, 500);
+            return;
+        }
+        dialog.innerHTML += Letters[positionLetter];
+        positionLetter++;
+    }
+}   
